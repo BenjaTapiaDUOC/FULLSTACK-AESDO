b0fc71083dbb84a16ecd4f35e8063bcff5429297
BenjaTapiaDUOC <benj.tapiad@duocuc.cl>
2026-07-02 04:47:04 -0400

    se agrega el gateaway con la parte de mia

---
+++ msgateway/src/test/java/com/tuapp/msgateway/MsgatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tuapp.msgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto de Spring (incluidas las rutas
+        // definidas en application.yml) levanta sin errores.
+    }
+
+}
